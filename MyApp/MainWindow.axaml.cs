8e689aa34278fcd52f49f85a1a2d52b524d967a3
ranonla <112718385+ranonla@users.noreply.github.com>
2025-12-13 13:17:02 -0500

    add NavigationService integration and page navigation to MainWindow

--- MyApp/MainWindow.axaml.cs
+++ MyApp/MainWindow.axaml.cs
@@ -1,11 +1,30 @@
 using Avalonia.Controls;
+using System.Collections.Generic;
+using MyApp.Services;
 
 namespace MyApp;
 
 public partial class MainWindow : Window
 {
+    private readonly NavigationService _navigation = new();
+
     public MainWindow()
     {
         InitializeComponent();
+
+        // Load initial page
+        Navigate(new Pages.HomePage());
+    }
+
+    public void Navigate(UserControl page)
+    {
+        _navigation.Navigate(page);
+        MainContent.Content = _navigation.CurrentPage;
+    }
+
+    public void GoBack()
+    {
+        _navigation.GoBack(new Pages.HomePage());
+        MainContent.Content = _navigation.CurrentPage;
     }
 }
