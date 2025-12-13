ae1118005623b3ffc74d5431fd83f62048f4ae3b
ranonla <112718385+ranonla@users.noreply.github.com>
2025-12-13 13:17:38 -0500

    create database table on application startup

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,18 +1,21 @@
 ﻿using Avalonia;
 using System;
+using LibraryCore;
 
 namespace MyApp;
 
 class Program
 {
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        // ⭐ Create database table BEFORE UI loads
+        LibraryCore.BookManager.createTable();
 
-    // Avalonia configuration, don't remove; also used by visual designer.
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
+
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
